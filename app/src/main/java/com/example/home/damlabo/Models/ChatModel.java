debd5dad199c39899ca193bbf6686cb389657aca
Home <Home@MacBook-Pro-de-Home.local>
2018-06-13 03:20:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/home/damlabo/Models/ChatModel.java
@@ -0,0 +1,36 @@
+package com.example.home.damlabo.Models;
+
+/**
+ * Created by Home on 13/06/18.
+ */
+
+public class ChatModel {
+
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+
+
+    public ChatModel() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+}
